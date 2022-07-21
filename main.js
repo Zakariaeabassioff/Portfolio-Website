dad5283733e1225ca652412af5398c1b085c5275
Zakariae ABASSI <abassi.zakariae7@gmail.com>
2022-07-21 16:02:20 +0100

    Email JS & sweetalert integration.

--- main.js
+++ main.js
@@ -3,3 +3,47 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
     menu.classList.toggle("move");
 };
+
+
+//Email JS
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+
+    sendBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(name.value == "" || email.value == "" || msg.value == ""){
+            emptyerror();
+        }else{
+            sendmail(name.value, email.value, msg.value);
+            success();
+        }
+    });
+}
+validate();
+
+function sendmail(name,email,msg){
+    emailjs.send("service_cllg3kn","template_l93293s",{
+        to_name: name,
+        from_name: email,
+        message: msg,
+});
+}
+
+function emptyerror(){
+    swal({
+        title: "Error!",
+        text: "Fields cannot be empty!",
+        icon: "error"
+    });
+}
+
+function success(){
+    swal({
+        title: "Email sent successfully!",
+        text: "Thanks for your Message! I'll reply asap!",
+        icon: "success"
+    });
+}
