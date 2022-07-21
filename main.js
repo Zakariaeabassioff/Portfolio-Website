08dca6f4ce4fb60756c06a3d8dddbbc8b1309786
Zakariae ABASSI <abassi.zakariae7@gmail.com>
2022-07-21 18:41:57 +0100

    Footer & change header background on scroll.

--- main.js
+++ main.js
@@ -47,3 +47,10 @@ function success(){
         icon: "success"
     });
 }
+
+//Header background change On Scroll
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-active', window.scrollY > 0);
+});
