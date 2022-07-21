e1f21197f2e4a3687ec510d4bec063bd223f9c67
Zakariae ABASSI <abassi.zakariae7@gmail.com>
2022-07-21 19:00:20 +0100

    Add scroll top button.

--- main.js
+++ main.js
@@ -54,3 +54,10 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('header-active', window.scrollY > 0);
 });
+
+//Scroll Top button
+
+let scrollTop = document.querySelector('.scroll-top');
+window.addEventListener('scroll', () => {
+    scrollTop.classList.toggle('scroll-active', window.scrollY >= 400);
+});
